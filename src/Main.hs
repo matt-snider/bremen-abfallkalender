b2302c1184a44459f3816d0f3a5483595d811462
Matt Snider <matt.snider@protonmail.com>
2019-11-09 13:37:48 +0100

    Use Category type instead of string for pickup variety

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Data.Char
+import           Data.List
 import           Data.Maybe
 import           Data.Time
 import           Network.HTTP
@@ -24,17 +25,38 @@ main = do
 -----------
 
 -- A date on which garbage or recycling will be picked up
-data PickupDate = PickupDate Day PickupInfo
+data PickupDate = PickupDate Day [Category]
 
--- The type of pickup (e.g. Bio / Müll, Recyling)
-type PickupInfo = String
+
+-- The type of pickup
+data Category
+    = Restmuell
+    | Bioabfall
+    | Papier
+    | GelberSack
+    | Tannenbaumabfuhr
+    | Other String
+    deriving (Ord, Read)
+
 
 --  A year is just an int
 type Year = Integer
 
--- Implement show to pretty print
+
 instance Show PickupDate where
-  show (PickupDate day info) = show day ++ " - " ++ info
+  show (PickupDate day cats) =
+        let catStr = intercalate ", " (map show cats)
+        in show day ++ " - " ++ catStr
+
+
+
+instance Show Category where
+  show Restmuell        = "Restmüll"
+  show Papier           = "Papier"
+  show Bioabfall        = "Bioabfall"
+  show GelberSack       = "Gelber Sack"
+  show Tannenbaumabfuhr = "Tannenbaumabfuhr"
+  show (Other str)      = '*' : str
 
 
 -------------
@@ -85,16 +107,20 @@ parsePage tags =
 -- Entries that begin with a day of the week in brackets indicate that the pickup
 -- occurs on an irregular day of the week (e.g. '(Sa.)' when pickup is usually Di.)
 parsePickupDate :: Year -> String -> PickupDate
-parsePickupDate year str =
-    let
-        (day, month) = parseDayMonth str
-        date         = fromGregorian year month day
-        isIrregular  = head str == '('
-        info         = if isIrregular
-            then drop 9 . dropWhile (/= ')') $ str
-            else drop 7 str
-    in
-        PickupDate date info
+parsePickupDate year str
+    = let
+          (day, month) = parseDayMonth str
+          date         = fromGregorian year month day
+          isIrregular  = head str == '('
+          infoStr      = if isIrregular
+              then drop 9 . dropWhile (/= ')') $ str
+              else drop 7 str
+          cats =
+              map (\s -> fromMaybe (Other s) (readMaybe s))
+                  . map normalizeCategory
+                  $ split infoStr '/'
+      in
+          PickupDate date cats
 
 
 -- The day and month are provided in the form DD.MM. (e.g. 31.10.)
@@ -102,6 +128,19 @@ parseDayMonth :: String -> (Int, Int)
 parseDayMonth s =
     let digits = filter isDigit s
     in  (read $ take 2 digits :: Int, read $ take 2 . drop 2 $ digits :: Int)
+
+
+-- Categories are shortened in the list and in order to use the
+-- derived Read instance of Cateogry we need to normalize them
+-- e.g. Restm. -> Restmuell, Restmüll -> Restmuell, Bioabf. -> Bioabfall, etc
+normalizeCategory :: String -> String
+normalizeCategory = translate . trim
+  where
+    translate "Restmüll"    = "Restmuell"
+    translate "Restm."      = "Restmuell"
+    translate "Bioabf."     = "Bioabfall"
+    translate "Gelber Sack" = "GelberSack"
+    translate x             = x
 
 
 -- Extract the text from a list of tags
@@ -137,3 +176,21 @@ getCalendar street streetNum =
         url    = "http://213.168.213.236/bremereb/bify/bify.jsp?" ++ params
     in  getResponseBody =<< simpleHTTP (getRequest url)
 
+
+-----------
+-- Utils --
+-----------
+
+-- Trim whitespace from beginning and end
+-- via https://stackoverflow.com/a/38283069
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
+
+
+-- Split a string based on a delimiter character
+-- via https://jmmv.dev/2006/08/split-function-in-haskell.html
+split :: String -> Char -> [String]
+split [] delim = [""]
+split (c : cs) delim | c == delim = "" : rest
+                     | otherwise  = (c : head rest) : tail rest
+    where rest = split cs delim
